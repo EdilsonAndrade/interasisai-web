d54a0eabad73d1a3697b98fc49520a87680b78e3
Edilson <edilson.a.andrade@gmail.com>
2026-04-21 17:04:18 -0300

    chore: setup framemotion

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,23 @@
 import "@testing-library/jest-dom";
+
+class MockIntersectionObserver implements IntersectionObserver {
+	readonly root: Element | Document | null = null;
+	readonly rootMargin = "0px";
+	readonly thresholds: ReadonlyArray<number> = [0];
+
+	disconnect(): void {}
+
+	observe(): void {}
+
+	takeRecords(): IntersectionObserverEntry[] {
+		return [];
+	}
+
+	unobserve(): void {}
+}
+
+Object.defineProperty(window, "IntersectionObserver", {
+	writable: true,
+	configurable: true,
+	value: MockIntersectionObserver,
+});
